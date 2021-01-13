afc273d7da3f114a901030b44af1ce5863516a2a
SofiaFia03 <74136529+SofiaFia03@users.noreply.github.com>
2021-01-13 00:57:29 -0800

    Add files via upload

---
+++ InterfaceImplement/src/com/dicoding/javafundamental/oop/Kucing.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.oop;
+
+/**
+ *
+ * @author USER
+ */
+public abstract class Kucing implements Hewan {
+
+    public void Hewan() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
